f33c84eed4c3269bc8730de2684e434a4e6e37bf
Emmanuel Toko <emmanuel.toko@gmail.com>
2013-07-25 16:02:00 +0300

    Added Form and FormConcept models

--- longitudinal_register/models/__init__.py
+++ longitudinal_register/models/__init__.py
@@ -250,6 +250,79 @@ class Group(Base, BaseEntity):
                #"modified_on": dump_datetime(self.modified_on)
                }
 
+
+
+
+
+class Person(Base):
+    """
+    Represents a person entity which directly represents a Patient
+    """
+
+    __tablename__ = "persons"
+
+    id = Column(u"id", Integer, Sequence("person_id_seq"), primary_key=True)
+    health_id = Column(u'health_id', String(20), unique = True)
+    surname = Column(u"surname", String(50), nullable = False)
+    other_names = Column(u"other_names", String(100), nullable = False)
+    gender = Column(u"gender", String(10), default = "FEMALE")
+    #make date_of_birth optional for the mother
+    dob = Column(u"dob", DateTime, server_default = text("now()")) 
+    dob_estimated = Column(Boolean, default=False)
+    status = Column(u"status", String(20)) # Entry point on workbook
+    location = Column(Integer, ForeignKey("locations.id"))
+    relations = relationship("Relationship", \
+        primaryjoin="Relationship.person_a==Person.id", \
+        lazy="join"
+        )
+    created_by = Column(u"created_by", ForeignKey("users.id"))
+    created_on = Column(u"created_on", DateTime, server_default = text("now()"))
+    modified_by = Column(u"modified_by", ForeignKey("users.id"))
+    modified_on = Column(u"modified_on", DateTime, server_default = text("now()"))
+ 
+    def __init__(self, *args, **kwargs):
+        self.surname = kwargs.pop("surname", None)
+
+
+class Patient(Base):
+    
+    __tablename__ = "patients"
+
+    id = Column(Integer, Sequence("patient_id_seq"), primary_key = True)
+    person = Column(Integer, ForeignKey("persons.id"))
+    voided = Column(Boolean, default = False)
+    created_by = Column(u"created_by", ForeignKey("users.id"))
+    created_on = Column(u"created_on", DateTime, server_default = text("now()"))
+    modified_by = Column(u"modified_by", ForeignKey("users.id"))
+    modified_on = Column(u"modified_on", DateTime, server_default = text("now()"))
+
+class Relationship(Base):
+
+    __tablename__ = "relationship"
+
+    id = Column(u"id", Integer, Sequence("relationship_id_seq"), primary_key=True)
+    type = Column(u"type", ForeignKey("relationship_type.id"), nullable = False)
+    person_a = Column(u"person_a", Integer, ForeignKey("persons.id"), nullable = False)
+    person_b = Column(u"person_b", Integer, ForeignKey("persons.id"), nullable = False)
+    created_by = Column(u"created_by", Integer, ForeignKey("users.id"), nullable = True)
+    created_on = Column(u"created_on", DateTime, server_default = text("now()"))
+    modified_by = Column(u"modified_by", Integer, ForeignKey("users.id"), \
+            nullable = True)
+    modified_on = Column(u"modified_on", DateTime, \
+            server_default = text("now()"), server_onupdate=text("now()"))
+
+class RelationshipType(Base):
+
+    __tablename__ = "relationship_type"
+
+    id = Column(u"id", Integer, Sequence("relationship_type_id_seq"), \
+            primary_key = True)
+    name = Column(u"name", String(50), nullable = False)
+    description = Column(u"description", String)
+
+
+
+
 class Permission(Base, BaseEntity):
     """
     Permissions - List of priviliges that are contained in the system
@@ -318,70 +391,6 @@ class HealthUnitType(Base):
     name = Column(String(60), nullable = False)
     description = Column(Text, nullable = True)
 
-
-class Person(Base, BaseExtension):
-    """
-    Represents a person entity which directly represents a Patient
-    """
-
-    __tablename__ = "persons"
-
-    id = Column(u"id", Integer, Sequence("person_id_seq"), primary_key=True)
-    health_id = Column(u'health_id', String(20), unique = True)
-    surname = Column(u"surname", String(50), nullable = False)
-    other_names = Column(u"other_names", String(100), nullable = False)
-    gender = Column(u"gender", String(10), default = "FEMALE")
-    #make date_of_birth optional for the mother
-    dob = Column(u"dob", DateTime, server_default = text("now()")) 
-    dob_estimated = Column(Boolean, default=False)
-    status = Column(u"status", String(20)) # Entry point on workbook
-    location = Column(Integer, ForeignKey("locations.id"))
-    created_by = Column(u"created_by", ForeignKey("users.id"))
-    created_on = Column(u"created_on", DateTime, server_default = text("now()"))
-    modified_by = Column(u"modified_by", ForeignKey("users.id"))
-    modified_on = Column(u"modified_on", DateTime, server_default = text("now()"))
- 
-    def __init__(self, *args, **kwargs):
-        self.surname = kwargs.pop("surname", None)
-
-
-class Patient(Base, BaseEntity):
-    
-    __tablename__ = "patients"
-
-    id = Column(Integer, Sequence("patient_id_seq"), primary_key = True)
-    person = Column(Integer, ForeignKey("persons.id"))
-    voided = Column(Boolean, default = False)
-    created_by = Column(u"created_by", ForeignKey("users.id"))
-    created_on = Column(u"created_on", DateTime, server_default = text("now()"))
-    modified_by = Column(u"modified_by", ForeignKey("users.id"))
-    modified_on = Column(u"modified_on", DateTime, server_default = text("now()"))
-
-class Relationship(Base, BaseEntity):
-
-    __tablename__ = "relationship"
-
-    id = Column(u"id", Integer, Sequence("relationship_id_seq"), primary_key=True)
-    type = Column(u"type", ForeignKey("relationship_type.id"), nullable = False)
-    person_a = Column(u"person_a", Integer, ForeignKey("persons.id"), nullable = False)
-    person_b = Column(u"person_b", Integer, ForeignKey("persons.id"), nullable = False)
-    created_by = Column(u"created_by", Integer, ForeignKey("users.id"), nullable = False)
-    created_on = Column(u"created_on", DateTime, server_default = text("now()"))
-    modified_by = Column(u"modified_by", Integer, ForeignKey("users.id"), \
-            nullable = False)
-    modified_on = Column(u"modified_on", DateTime, \
-            server_default = text("now()"), server_onupdate=text("now()"))
-
-class RelationshipType(Base, BaseEntity):
-
-    __tablename__ = "relationship_type"
-
-    id = Column(u"id", Integer, Sequence("relationship_type_id_seq"), \
-            primary_key = True)
-    name = Column(u"name", String(50), nullable = False)
-    description = Column(u"description", String)
-
-
 class Concept(Base):
 
     __tablename__ = "concepts"
@@ -439,7 +448,7 @@ class Visit(Base):
     __tablename__ = "visits"
 
     id = Column(Integer, Sequence("visit_id_seq"), primary_key = True)
-    visit_type = Column(Integer, ForeignKey("visit_types.id"))
+    form = Column(Integer, ForeignKey("forms.id"))
     patient = Column(Integer, ForeignKey("patients.id"), nullable = False)
     provider = Column(Integer, ForeignKey("users.id"))
     health_unit = Column(Integer, ForeignKey("health_units.id"))
@@ -451,23 +460,24 @@ class Visit(Base):
     voided = Column(Boolean, default = False)
     observations = relationship("Observation", order_by="Observation.id")
 
-class VisitType(Base):
-
-    __tablename__ = "visit_types"
-
-    id = Column(Integer, Sequence("visit_type_id_seq"), primary_key = True)
-    name = Column(String(50), nullable = False)
-    description = Column(Text)
-    created_by = Column(Integer, ForeignKey("users.id"))
-    created_on = Column(DateTime, server_default = text("now()"))
-    modified_by = Column(Integer, ForeignKey("users.id"))
-    modified_on = Column(DateTime, server_default = text("now()"))
-    voided = Column(Boolean, default = False)
+#class VisitType(Base):
+#
+#    __tablename__ = "visit_types"
+#
+#    id = Column(Integer, Sequence("visit_type_id_seq"), primary_key = True)
+#    name = Column(String(50), nullable = False)
+#    description = Column(Text)
+#    created_by = Column(Integer, ForeignKey("users.id"))
+#    created_on = Column(DateTime, server_default = text("now()"))
+#    modified_by = Column(Integer, ForeignKey("users.id"))
+#    modified_on = Column(DateTime, server_default = text("now()"))
+#    voided = Column(Boolean, default = False)
 
 
 class Observation(Base):
 
     __tablename__ = "observations"
+    __table_args = (UniqueConstraint("visit", "concept"),)
 
     id = Column(Integer, Sequence("observation_id_seq"), primary_key = True)
     visit = Column(Integer, ForeignKey("visits.id"))
@@ -481,5 +491,29 @@ class Observation(Base):
             server_onupdate = text("now()"))
     voided = Column(Boolean, default = False) 
 
-    __table_args = (UniqueConstraint("visit", "concept"),)
+
+class Form(Base):
+
+    __tablename__ = "forms"
+
+    id = Column(Integer, Sequence("form_id_seq"), primary_key=True)
+    name = Column(String(30))
+    version = Column(String(20))
+    description = Column(Text)
+    concepts = relationship("FormConcept")
+    created_by = Column(Integer, ForeignKey("users.id"))
+    created_on = Column(DateTime, server_default = text("now()"))
+    modified_by = Column(Integer, ForeignKey("users.id"))
+    modified_on = Column(DateTime, server_default = text("now()"), \
+        server_onupdate = text("now()"))
+
+
+class FormConcept(Base):
+
+    __tablename__ = "form_concepts"
+    __table_args = (UniqueConstraint("form", "concept",))
+
+    id = Column(Integer, Sequence("form_concept_id"), primary_key=True)
+    form = Column(Integer, ForeignKey("forms.id"))
+    concept = Column(Integer, ForeignKey("concepts.id"))
 
