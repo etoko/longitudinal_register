9e957031ebe1ced89433fbee7e11faf208e7c5da
Emmanuel Toko <emmanuel.toko@gmail.com>
2013-09-21 23:07:02 +0300

    Functionalised saving of forms

--- longitudinal_register/views/visits.py
+++ longitudinal_register/views/visits.py
@@ -1,4 +1,5 @@
 import json
+import transaction
 
 from pyramid.httpexceptions import (
   exception_response,
@@ -15,19 +16,23 @@ from pyramid.view import view_config
 from formencode import Schema
 from formencode import validators
 
+from pyramid_simpleform import Form
+
 from longitudinal_register.models import Visit
+from longitudinal_register import models
 
 class VisitSchema(Schema):
-    id = validators.Int()
-    visit_type = validators.Int()
-    patient = validators.Int()
-    provider = validators.Int()
+    allow_extra_fields =  True
+    form_id = validators.Int()
+    #visit_type = validators.Int()
+    health_id = validators.Int()
+    #provider = validators.Int()
     health_unit = validators.Int()
     visit_date = validators.DateValidator()
-    created_by = validators.Int()
-    created_date = validators.DateValidator()
-    modified_by = validators.Int()
-    modified_on = validators.DateValidator()
+    #created_by = validators.Int()
+    #created_date = validators.DateValidator()
+    #modified_by = validators.Int()
+    #modified_on = validators.DateValidator()
     
 @view_config(route_name="visit_list", renderer="json")
 def visit_list(request):
@@ -43,6 +48,50 @@ def visit_view(request):
         visit = form.bind(Visit())
         with transaction.manager:
             DBSession.add(visit)
+
+
+@view_config(route_name="visit_save", renderer="json")
+def visit_save_view(request):
+    health_id = request.matchdict['health_id']
+    form_id = request.matchdict["form"]
+    visit_date = request.POST["visit_date"]
+    health_unit = request.POST["health_unit"]
+    observations = []
+
+    person = None
+    with transaction.manager:
+        person = models.DBSession.query(models.Person).\
+            filter(models.Person.health_id == health_id).first()
+
+    form = Form(request, schema=VisitSchema())
+    if form.validate():
+        for param,value in request.POST.iteritems():
+            try:
+                param =int(param)
+                print param, value,'\n'
+                with transaction.manager:
+                    concept = models.DBSession.query(models.Concept).get(param)
+                    if concept:
+                        observation = models.Observation()
+                        observation.concept = param
+                        observation.concept_value
+                        observations.append(observation)
+            except ValueError:
+                continue
+        
+        visit = form.bind(models.Visit())
+        visit.person = person.id
+        visit.observations = observations
+        models.DBSession.add(visit)
+        models.DBSession.flush()
+        #with transaction.manager:
+        #    visit.observations = observations
+        #    models.DBSession.merge(visit)
+    else:
+        print form.all_errors()
+    print request.POST
+    return HTTPFound(request.route_url("patients_list_page"))
+   
 
 def visit_render_form(request):
     pass
